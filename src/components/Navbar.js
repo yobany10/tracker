e88134e322a53cd58d9cf4936ba3c8773ee2ccfe
yobany10 <yobanydesign@gmail.com>
2026-03-19 22:28:10 -0400

    Add initial routing

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <div>
+            <p>This is the navbar for now</p>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+const Home = () => {
+    return (
+        <p>This is the home page for now</p>
+    )
+}
+
+export default Home;
